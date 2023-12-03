30f8e8e0589c6746cd468c1b16e7ba05b2aa8647
William Lee <wlee753159@gmail.com>
2023-12-03 00:26:24 +0000

    Improve Day 1 solutions
    Only look for first and last digit rather than collect all digits

--- days/01/part1.rb
+++ days/01/part1.rb
@@ -1,9 +1,11 @@
 # Sum over the number calculated from each line
 answer = File.readlines('input.txt', chomp: true).sum do |line|
   # Take all characters that are digits for each line into an array
-  digits = line.chars.filter{ |char| char =~ /\d+/ }
+  first_digit = line.match(/^\D*?(\d)/)[1]
+  last_digit = line.match(/^.*(\d)\D*?$/)[1]
+
   # Join the first and last one
-  "#{digits.first}#{digits.last}".to_i
+  "#{first_digit}#{last_digit}".to_i
 end
 
 puts answer
--- days/01/part2.rb
+++ days/01/part2.rb
@@ -1,5 +1,6 @@
 NUMBERS = %w(one two three four five six seven eight nine)
-DIGIT_REGEX = /^(\d|#{NUMBERS.join('|')})/
+FIRST_DIGIT_REGEX = /^\w*?(\d|#{NUMBERS.join('|')})/
+LAST_DIGIT_REGEX = /^.*(\d|#{NUMBERS.join('|')})\w*?$/
 
 def map_digit(digit)
   (digit =~ /\d/) ? digit : "#{NUMBERS.index(digit) + 1}"
@@ -7,17 +8,12 @@ end
 
 # Sum over the number calculated from each line
 answer = File.readlines('input.txt', chomp: true).sum do |line|
-  # Starting from each character on the line.
-  # This ensures that overlapping digits are captured.
-  digits = (0...line.length)
-    .map do |start|
-      line[start..line.length].scan(DIGIT_REGEX)
-    end.flatten
-    # Map these digits to the numeral digit
-    .map{ |digit| map_digit(digit) }
+  first_digit = map_digit(line.match(FIRST_DIGIT_REGEX)[1])
+  last_digit = map_digit(line.match(LAST_DIGIT_REGEX)[1])
 
   # Join the first and last digits
-  "#{digits.first}#{digits.last}".to_i
+  puts "#{first_digit}#{last_digit}"
+  "#{first_digit}#{last_digit}".to_i
 end
 
 puts answer
